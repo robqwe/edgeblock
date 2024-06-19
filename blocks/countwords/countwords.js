4873c861eb01b07522d6370880f2be9f6a84c842
robqwe <robinbussell@gmail.com>
2024-06-19 12:10:45 +0100

    removed countwords

--- blocks/countwords/countwords.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-    const text = document.body.innerText || document.body.textContent;
-
-    const words = text.match(/\b\w+\b/g);
-
-    const wordCount = words ? words.length : 0;
-}
